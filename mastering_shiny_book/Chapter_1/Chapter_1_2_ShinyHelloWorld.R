a32ffb4adc6d9f2dc9310ac58a32873c8067cd86
redz <redzu.m@gmail.com>
2024-05-13 19:34:58 +0800

    Reproducing chapter 1 shiny apps.

---
+++ mastering_shiny_book/Chapter_1/Chapter_1_2_ShinyHelloWorld.R
@@ -0,0 +1,13 @@
+library(shiny)
+
+ui <- fluidPage(
+    "Hello, World"
+)
+
+
+server <- function(input, output, session){
+    
+}
+
+
+shinyApp(ui, server)
---
+++ mastering_shiny_book/Chapter_1/Chapter_1_6_adding_behaviour_reactive.R
@@ -0,0 +1,34 @@
+library(shiny)
+
+ui <- fluidPage(
+    selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+    verbatimTextOutput("summary"),
+    tableOutput("table")
+)
+
+
+server <- function(input, output, session){
+    
+    dataset <- reactive({
+        get(input$dataset, "package:datasets")
+    })
+    
+    output$summary <- renderPrint({
+        #dataset <-  get(input$dataset, "package:datasets")
+        summary(dataset())
+        
+    })
+    
+    output$table <- renderTable({
+        #dataset <- get(input$dataset, "package:datasets")
+        dataset()
+    })
+        
+        
+        
+       
+    
+}
+
+
+shinyApp(ui, server)
